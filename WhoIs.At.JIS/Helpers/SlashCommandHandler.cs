dc1bcf60e0be88e3142f0676d87287789ce32296
Brendon Thiede <ThiedeB@courts.mi.gov>
2019-01-27 00:33:30 -0500

    Adding more helpers

--- WhoIs.At.JIS/Helpers/SlashCommandHandler.cs
+++ WhoIs.At.JIS/Helpers/SlashCommandHandler.cs
@@ -4,12 +4,17 @@ using Microsoft.Identity.Client;
 using System.Collections.Generic;
 using WhoIs.At.JIS.Models;
 using System;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 
 namespace WhoIs.At.JIS.Helpers
 {
 
   public static class SlashCommandHandler
   {
+    static bool isUpdating = false;
     static List<string> VALID_COMMANDS = new List<string> { "help", "email", "name" };
     static string[] CONFIG_VARIABLES = new string[] { "applicationId", "applicationSecret", "redirectUri", "tenantId", "domain" };
 
@@ -26,12 +31,25 @@ namespace WhoIs.At.JIS.Helpers
       return VALID_COMMANDS.Contains(command);
     }
 
-    public static bool isEmail(string email)
+    public static bool isValidEmail(string email)
+    {
+      return isValidEmail(email, null);
+    }
+
+    public static bool isValidEmail(string email, string domain)
     {
       try
       {
         var addr = new System.Net.Mail.MailAddress(email);
-        return addr.Address == email;
+        if (String.IsNullOrEmpty(domain))
+        {
+          return addr.Address == email;
+
+        }
+        else
+        {
+          return addr.Address == email && addr.Host == domain;
+        }
       }
       catch
       {
@@ -53,7 +71,7 @@ namespace WhoIs.At.JIS.Helpers
           tmp.RemoveAt(0);
           whoIsCommand.parameters = tmp.ToArray();
         }
-        else if (isEmail(whoIsCommand.command))
+        else if (isValidEmail(whoIsCommand.command))
         {
           whoIsCommand.parameters = new string[] { whoIsCommand.command };
           whoIsCommand.command = "email";
@@ -64,6 +82,106 @@ namespace WhoIs.At.JIS.Helpers
         }
       }
       return whoIsCommand;
+    }
+
+    public static List<GraphUser> getAllMsGraphUsers()
+    {
+      GraphServiceClient graphClient = GetAuthenticatedGraphClient(getAuthConfig());
+      List<GraphUser> allUsers = new List<GraphUser>();
+      for (char letter = 'A'; letter <= 'Z'; letter++)
+      {
+        Console.WriteLine($"*** Pulling users for {letter}");
+        List<QueryOption> options = new List<QueryOption>
+        {
+            new QueryOption("$filter", $"startsWith(mail,'{letter}')"),
+            new QueryOption("$select", "userPrincipalName")
+        };
+        try
+        {
+          var graphResult = graphClient.Users.Request(options).GetAsync().Result;
+          var retVal = new List<string>();
+          foreach (var result in graphResult)
+          {
+            List<QueryOption> userOptions = new List<QueryOption>
+          {
+              new QueryOption("$select", "displayName,jobTitle,userPrincipalName,interests,skills,pastProjects,aboutMe")
+          };
+            try
+            {
+              if (isValidEmail(result.UserPrincipalName, "courts.mi.gov"))
+              {
+                GraphUser graphUser = asGraphUser(graphClient.Users[result.UserPrincipalName].Request(userOptions).GetAsync().Result);
+                if (!String.IsNullOrEmpty(graphUser.jobTitle))
+                {
+                  allUsers.Add(asGraphUser(graphClient.Users[result.UserPrincipalName].Request(userOptions).GetAsync().Result));
+                }
+
+              }
+            }
+            catch (System.Exception)
+            {
+              System.Console.WriteLine($"*** Error encountered trying to pull {result.UserPrincipalName}");
+            }
+          }
+        }
+        catch (System.Exception)
+        {
+
+          System.Console.WriteLine($"*** Error pulling emails starting with {letter}");
+        }
+      }
+      return allUsers;
+    }
+
+    async public static void updateUserCache()
+    {
+      if (!isUpdating)
+      {
+        Stopwatch watch = new Stopwatch();
+        watch.Start();
+        //Do things
+        isUpdating = true;
+        Console.WriteLine("*** Starting update process");
+        await Task.Run(() =>
+        {
+          var allUsers = getAllMsGraphUsers();
+          string json = JsonConvert.SerializeObject(allUsers.ToArray());
+
+          //write string to file
+          System.IO.File.WriteAllText("snapShot.json", json);
+          watch.Stop();
+          Console.WriteLine($"*** Update ran for {watch.Elapsed.TotalSeconds.ToString()} seconds");
+          isUpdating = false;
+        });
+      }
+      else
+      {
+        Console.WriteLine("*** Update was already running");
+      }
+    }
+
+    public static List<GraphUser> getCachedUsers()
+    {
+      using (StreamReader r = new StreamReader("snapShot.json"))
+      {
+        string json = r.ReadToEnd();
+        return JsonConvert.DeserializeObject<List<GraphUser>>(json);
+      }
+
+    }
+
+    private static GraphUser asGraphUser(User user)
+    {
+      return new GraphUser
+      {
+        displayName = user.DisplayName,
+        jobTitle = user.JobTitle,
+        userPrincipalName = user.UserPrincipalName,
+        aboutMe = user.AboutMe,
+        interests = user.Interests,
+        skills = user.Skills,
+        pastProjects = user.PastProjects
+      };
     }
 
     public static List<string> getMsGraphResultsForName(string name)
@@ -79,7 +197,7 @@ namespace WhoIs.At.JIS.Helpers
       var retVal = new List<string>();
       foreach (var result in graphResult)
       {
-        retVal.Add($"{result.DisplayName} - {result.UserPrincipalName}");
+        retVal.Add($"{result.DisplayName} - {result.JobTitle} - {result.Mail}");
       }
       return retVal;
     }
@@ -90,11 +208,27 @@ namespace WhoIs.At.JIS.Helpers
       List<QueryOption> options = new List<QueryOption>
         {
             new QueryOption("$top", "10"),
-            new QueryOption("$select", "displayName,jobTitle,userPrincipalName,interests,skills,pastProjects,aboutMe")
+            new QueryOption("$select", "displayName,jobTitle,mail,interests,skills,pastProjects,aboutMe")
         };
 
       var graphResult = graphClient.Users[email].Request(options).GetAsync().Result;
-      return $"{graphResult.DisplayName} - {graphResult.UserPrincipalName}";
+      var profileData = $"{graphResult.DisplayName}\n{graphResult.JobTitle}\n{graphResult.Mail}";
+      if (!string.IsNullOrEmpty(graphResult.AboutMe)) {
+        profileData += $"\n>{graphResult.AboutMe}";
+      }
+      var pastProjects = new List<string>(graphResult.PastProjects);
+      if (pastProjects.Count > 0) {
+        profileData += $"\nProjects:\n  {string.Join("\n  ", graphResult.PastProjects)}";
+      }
+      var skills = new List<string>(graphResult.Skills);
+      if (skills.Count > 0) {
+        profileData += $"\nSkills:\n  {string.Join("\n  ", graphResult.Skills)}";
+      }
+      var interests = new List<string>(graphResult.Interests);
+      if (interests.Count > 0) {
+        profileData += $"\nInterests:\n  {string.Join("\n  ", graphResult.Interests)}";
+      }
+      return profileData;
     }
 
     private static Dictionary<string, string> getAuthConfig()
